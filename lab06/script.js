adc4e20185099d763eb7e93a5b941db402914db6
yazan <yazan.alshekha@outlook.com>
2020-05-02 14:23:03 +0300

    add lab07

--- lab06/script.js
+++ lab06/script.js
@@ -1,23 +1,34 @@
 'use strict'
-confirm('Hello our customer')
-var name = prompt('enter you name ');
+confirm('Hello our customer');
+getTime();
 
-var today = new Date();
-var min = today.getUTCMinutes()
-var hourNow = today.getHours();
-var greeting;
-if (hourNow > 18) {
-  greeting = 'Good evening';
-} else if (hourNow > 12) {
-  greeting = 'Good afternoon';
-} else if (hourNow >= 0) {
-  greeting = 'Good morning';
-} else {
-  greeting = 'Wellcome';
+function getName(){
+  var name = prompt('enter you name ');
+  return name
 }
-//document.write('<h3>'+greeting+'</h3>');
 
-document.getElementById('time').innerHTML = 'your clock is ' + hourNow + ':' + min;
 
-document.getElementById("thename").innerHTML = greeting + ' ' + name;
+function getTime(){
+  var today = new Date();
+  var min = today.getUTCMinutes()
+  var hourNow = today.getHours();
+  var greeting;
+  if (hourNow > 18) {
+    greeting = 'Good evening';
+  } else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+  } else if (hourNow >= 0) {
+    greeting = 'Good morning';
+  } else {
+    greeting = 'Wellcome';
+  }
+  document.getElementById('time').innerHTML = 'your clock is ' + hourNow + ':' + min;
+  document.getElementById("thename").innerHTML = greeting + ' ' + getName();
+  //document.write('<h3>'+greeting+'</h3>');
+}
+
+
+
+
+
 
